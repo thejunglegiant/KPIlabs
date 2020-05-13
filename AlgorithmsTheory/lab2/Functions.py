ad23170d8dcb41082c4c0567d349b3cbf1d96a79
Oleksii <oleksii.m02@gmail.com>
2020-05-13 21:43:41 +0300

    .gitignore edited

--- AlgorithmsTheory/lab2/Functions.py
+++
@@ -1,9 +0,0 @@
-import InsertionSort
-import MergeSort
-
-
-def hybrid_sort(array):
-    if len(array) < 20:
-        return InsertionSort.sort(array)
-    else:
-        return MergeSort.sort(array)
